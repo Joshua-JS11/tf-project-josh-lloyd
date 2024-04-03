a36973cc57904065b534acc5b451431e144569ce
Lloyd <lloyd585@outlook.com>
2024-04-03 16:34:15 +0100

    variables created to store EC2 and S3 bucket variables

---
+++ variables.tf
@@ -0,0 +1,9 @@
+# Creates the variable names with a description
+
+variable "my_bucket" {
+  description = "Name of the S3 bucket"
+}
+
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+}
